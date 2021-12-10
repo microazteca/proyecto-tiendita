920ecf26e343f6562c0151d8a84c58ad54502929
Betania <aguilarperezbetania@gmail.com>
2021-12-10 00:21:42 -0500

    building services section

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,4 +1,33 @@
 <template lang="pug">
-	.services
-		h1.services__h1 Holi, ésta es la sección de Servicios
+.services
+  h1.services__h1 Servicios
+  ServiceBar
+    slot(slot="servicio") Pagos
+  ServiceBar
+    slot(slot="servicio") Recargas
+  ServiceBar
+    slot(slot="servicio") Copias e impresiones
+  ServiceBar
+    slot(slot="servicio") Retiros
 </template>
+<script>
+import ServiceBar from "@/components/molecules/ServiceBar.vue";
+import IconMobile from "@/components/atoms/IconMobile.vue";
+
+export default {
+  components: {
+    ServiceBar,
+    IconMobile,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.services {
+  margin: 10px 0;
+  padding: 0 16px;
+  &__h1 {
+    font-size: 24px;
+  }
+}
+</style>
---
+++ src/components/molecules/ServiceBar.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+.bar
+  .bar__container
+    slot(name="src")
+    p.bar__text
+      slot(name="servicio")
+  DownArrowServices.bar__arrow
+  UpArrowServices.bar__arrow.bar__arrow--up
+</template>
+
+<script>
+import DownArrowServices from "@/components/atoms/DownArrowServices.vue";
+import UpArrowServices from "@/components/atoms/UpArrowServices.vue";
+export default {
+  props: {
+    src: { type: String },
+  },
+  components: {
+    UpArrowServices,
+    DownArrowServices,
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Manrope:wght@600&display=swap");
+.bar {
+  display: flex;
+  justify-content: space-between;
+  &__container {
+    display: flex;
+    align-self: flex-end;
+    gap: 5px;
+  }
+
+  &__svg {
+    color: var(--icon-services-color);
+  }
+
+  &__text {
+    font-size: 20px;
+    font-family: "Manrope", sans-serif;
+  }
+
+  &__arrow {
+    color: var(--icon-services-color);
+
+    &--up{
+      display: none;
+    }
+  }
+}
+</style>
