1d2922b174fa26608c9df45c951719879145622a
Betania <aguilarperezbetania@gmail.com>
2022-03-18 12:29:20 -0500

    deleting the g-c container

--- src/components/home/Services.vue
+++ src/components/home/Services.vue
@@ -2,74 +2,69 @@
   .services
     h2.title Servicios
     .grid
-      .g-c
-        Accordion.accordion
-          slot(slot="icon")
-            IconCash
-          slot(slot="servicio") Pagos
-          slot(slot="img")
-            .container
-              img(src="@/assets/img/pagos/aguakan.png")
-          slot(slot="img")
-            .container
-              img(src="@/assets/img/pagos/cfe.png")
-          slot(slot="img")
-            .container
-              img(src="@/assets/img/pagos/dish.png")
-          slot(slot="img")
-            .container
-              img(src="@/assets/img/pagos/izzi.png")
-          slot(slot="img")
-            .container
-              img(src="@/assets/img/pagos/netflix.png")
-          slot(slot="img")
-            .container
-              img(src="@/assets/img/pagos/infonavit.png")
-          slot(slot="img")
-            .container
-              img(src="@/assets/img/pagos/total-play.svg")
-      .g-c
-        Accordion.accordion
-          slot(slot="icon")
-            IconMobile
-          slot(slot="servicio") Recargas
-          slot(slot="img")
-            .container
-              img(src="@/assets/img/recargas/att.png")
-          slot(slot="img")
-            .container
-              img(src="@/assets/img/recargas/movistar.png")
-          slot(slot="img")
-            .container
-              img(src="@/assets/img/recargas/telcel.png")
-          slot(slot="img")
-            .container
-              img(src="@/assets/img/recargas/unefon.png")
-      .g-c
-        Accordion.accordion
-          slot(slot="icon")
-            IconPrinter
-          slot(slot="servicio") Copias e impresiones
-          slot(slot="impresiones")
-            .container
-              li Acta de nacimiento
-              li CURP
-              li RFC
-              li Citas INE
-              li Citas IMSS
-              li Citas pasaporte
-              li Boletas de calificaciones
-      .g-c
-        Accordion.accordion
-          slot(slot="icon")
-            IconCard
-          slot(slot="servicio") Retiros de efectivo
-          slot(slot="img")
-            .container
-              img(src="@/assets/img/retiros/master-card.svg")
-          slot(slot="img")
-            .container
-              img(src="@/assets/img/retiros/visa.png")
+      Accordion.accordion
+        slot(slot="icon")
+          IconCash
+        slot(slot="servicio") Pagos
+        slot(slot="img")
+          .container
+            img(src="@/assets/img/pagos/aguakan.png")
+        slot(slot="img")
+          .container
+            img(src="@/assets/img/pagos/cfe.png")
+        slot(slot="img")
+          .container
+            img(src="@/assets/img/pagos/dish.png")
+        slot(slot="img")
+          .container
+            img(src="@/assets/img/pagos/izzi.png")
+        slot(slot="img")
+          .container
+            img(src="@/assets/img/pagos/netflix.png")
+        slot(slot="img")
+          .container
+            img(src="@/assets/img/pagos/infonavit.png")
+        slot(slot="img")
+          .container
+            img(src="@/assets/img/pagos/total-play.svg")
+      Accordion.accordion
+        slot(slot="icon")
+          IconMobile
+        slot(slot="servicio") Recargas
+        slot(slot="img")
+          .container
+            img(src="@/assets/img/recargas/att.png")
+        slot(slot="img")
+          .container
+            img(src="@/assets/img/recargas/movistar.png")
+        slot(slot="img")
+          .container
+            img(src="@/assets/img/recargas/telcel.png")
+        slot(slot="img")
+          .container
+            img(src="@/assets/img/recargas/unefon.png")
+      Accordion.accordion
+        slot(slot="icon")
+          IconPrinter
+        slot(slot="servicio") Copias e impresiones
+        slot(slot="impresiones")
+          .container
+            li Acta de nacimiento
+            li CURP
+            li RFC
+            li Citas INE
+            li Citas IMSS
+            li Citas pasaporte
+      Accordion.accordion
+        slot(slot="icon")
+          IconCard
+        slot(slot="servicio") Retiros de efectivo
+        slot(slot="img")
+          .container
+            img(src="@/assets/img/retiros/master-card.svg")
+        slot(slot="img")
+          .container
+            img(src="@/assets/img/retiros/visa.png")
 </template>
 <style lang="scss" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@600&display=swap');
@@ -99,15 +94,9 @@
   }
 }
 
-.g-c{
-  @media (min-width: 1024px){
-    width: 80%;
-  }
-}
-
 .accordion{
   @media (min-width: 1024px){
-    width: 100%;
+    width: 80%;
   }
 }
 
